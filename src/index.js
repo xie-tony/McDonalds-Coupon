e46fba851cdf624fbb0309b8a0a1339fd4faa3c1
Tony Xie <651376379@qq.com>
2018-05-24 23:53:41 -0400

    Added web server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const parse = require('./parsePdf.js');
 const util = require('./Util.js');
 const fs = require('fs-extra');
 const md5File = require('md5-file');
+const Koa = require('koa');
+const Router = require('koa-router');
+
+var app = new Koa();
+var router = new Router();
+
+var cached = [];
 
 const pdfPath = './pdf/ON_Mailer.pdf';
 async function fetchAndParse() {
@@ -12,11 +19,21 @@ async function fetchAndParse() {
     await util.downloadFile(url, pdfPath);
     if (fileExist && util.sameFile(pdfPath, pdfPath+'.bak')){
         console.log('No new coupons found');
-        return;
+        return cached;
     }
     let text = await parse.parsePdf(pdfPath);
-    console.log(JSON.stringify(text));
+    console.log('New coupons fetched');
+    cached = text;
     return text;
 }
 
-fetchAndParse();
+router.get('/getCoupons', async ctx => {
+    let text = await fetchAndParse();
+    ctx.status = 200;
+    ctx.body = text;
+});
+
+app.use(router.routes())
+   .use(router.allowedMethods());
+
+app.listen(3060, () => console.log('App listening on 3060'));
